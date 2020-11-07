0bdd8e7cebfaaf6ec7a9917f220ad9610c51c73c
besenio <kbesenio2@yahoo.com>
2020-11-07 09:20:41 -0800

    home component styling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,21 @@ import './Home.css';
 function Home() {
    return (
       <div className="home">
-         <div className="home-container"></div>
+         <div className="home-container">
+            <img className="home-image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"/>
+         </div>
+
+         <div className="home-row">
+
+         </div>
+
+         <div className="home-row">
+
+         </div>
+         
+         <div className="home-row">
+            
+         </div>
       </div>
    )
 }
